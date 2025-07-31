2854b3450fae8753228dd8cbf2a9f78cd95d150f
Georgia <georgialloydphoto@gmail.com>
2025-07-30 23:58:53 -0500

    Work on responsive design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,21 @@
 import "./Footer.css";
-import GitHub from "../../assets/github-logo.png";
-import Facebook from "../../assets/facebook-logo.png";
 
 function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copyright">
-        &copy; 2024 Supersite, Powered by News APi
+        &copy; 2024 Supersite, Powered by News API
       </p>
       <div className="footer__navigation">
-        <p className="footer__navigation__link">Home</p>
-        <p className="footer__navigation__link">TripleTen</p>
+        <div className="footer__navigation__link-container">
+          <p className="footer__navigation__link">Home</p>
+          <p className="footer__navigation__link">TripleTen</p>
+        </div>
         <a href="https://github.com/joja-peaches">
-          <img
-            src={GitHub}
-            alt="GitHub Logo"
-            className="footer__navigation__social"
-          />
+          <div className="footer__navigation__github" />
         </a>
         <a href="https://www.facebook.com/georgialloydphoto">
-          <img
-            src={Facebook}
-            alt="Facebook Logo"
-            className="footer__navigation__social"
-          />
+          <div className="footer__navigation__facebook" />
         </a>
       </div>
     </footer>
